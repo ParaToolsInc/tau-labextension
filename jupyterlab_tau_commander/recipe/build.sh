129d2ef01aa8e5201d10545c590f340836d1dc16
PlatinumCD <Cameronfdurbin@gmail.com>
2022-03-14 17:49:54 -0500

    Add github location

--- jupyterlab_tau_commander/recipe/build.sh
+++ jupyterlab_tau_commander/recipe/build.sh
@@ -2,6 +2,7 @@
 $PYTHON -m pip install --no-deps --ignore-installed .
 
 export JUPYTERLAB_DIR=$PREFIX/share/jupyter/tau_commander
+export EXTENSION_LOCATION=/home/runner/work/tau-labextension/tau-labextension
 
 # Extensions to install
 export NODE_OPTIONS=--max-old-space-size=16000
@@ -9,6 +10,6 @@ export NODE_OPTIONS=--max-old-space-size=16000
 jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
 jupyter labextension install jupyterlab-plotly --no-build
 jupyter labextension install jupyterlab-dash --no-build
-jupyter labextension install . --no-build
+jupyter labextension install $EXTENSION_LOCATION --no-build
 jupyter lab build --debug
 #jupyter lab clean
